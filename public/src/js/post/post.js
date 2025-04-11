b4ac663dfc505510691b99ab42e462f3d4ac7cc0
Abhishek Gupta <cryprincesis007@gmail.com>
2025-04-11 12:10:05 +0530

    logo updation

--- public/src/js/post/post.js
+++ public/src/js/post/post.js
@@ -1,97 +1,106 @@
 
-let obj ={
-	title : '',
-	content :'',
+let obj = {
+	title: '',
+	content: '',
 }
 
-function setObject(tag){
-	if(tag.name=="title" && tag.value != ''){
+function setObject(tag) {
+	if (tag.name == "title") {
+		if (tag.value === '') return toast.toastError(`${tag.name} Field Required`)
 		obj.title = tag.value
 	}
-	if(tag.name=="content" && tag.value != ''){
+	if (tag.name == "content") {
+		if (tag.value === '') return toast.toastError(`${tag.name} Field Required`)
 		obj.content = tag.value
 	}
-	if(tag.name == 'tags' && tag.value != ''){
+	if (tag.name == 'tags') {
 		obj.tags = tag.value
 	}
 }
 
-async function post(event){
+async function post(event) {
 	event.preventDefault()
 	obj['date'] = new Date().toISOString();
-  	obj.likes = []
-  	obj.tags = obj.tags.split(',')
+	obj.likes = []
+	obj.tags = obj?.tags?.split(',')
+	loader()
 	const res = await fetch('https://blogejs-magw.onrender.com/post/create', {
 
-		method : 'POST',
-		headers:{
-			'content-type':'application/json'
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json'
 		},
-		body:JSON.stringify({...obj})
+		body: JSON.stringify({ ...obj })
 	})
+	removeLoader()
 	const data = await res.json()
-		toast.toastSuccess(data.msg)
-	if(data.success){
+	toast.toastSuccess(data.msg)
+	if (data.success) {
 		toast.toastSuccess(data.msg)
 		window.location.href = '/'
 	}
-	else{
+	else {
 		toast.toastError(data.msg)
 		console.log(data)
 	}
 }
 
-async function update(event, user_id, pid){
+async function update(event, user_id, pid) {
 	event.preventDefault()
 	obj['date'] = new Date()
-
+	loader()
 	const res = await fetch(`https://blogejs-magw.onrender.com/post/update/${pid}`, {
 
-		method : 'put',
-		headers:{
-			'content-type':'application/json'
+		method: 'put',
+		headers: {
+			'content-type': 'application/json'
 		},
-		body:JSON.stringify({...obj})
+		body: JSON.stringify({ ...obj })
 	})
+	removeLoader()
 	const data = await res.json()
-	if(data.success){
+	if (data.success) {
 		toast.toastSuccess(data.msg)
 		window.location.href = '/'
 	}
-	else{
+	else {
 		toast.toastError(data.msg)
 		console.log(data)
 	}
 }
 
 
-function redirect(event){
+function redirect(event) {
 	event.preventDefault()
 	toast.toastInfo('You have been redirected to homepage')
 	window.location.href = '/'
 }
 
 
-async function deleteAction(event, id){
+async function deleteAction(event, id) {
 	event.preventDefault()
 	const url = `https://blogejs-magw.onrender.com/post/delete/${id}`
 	console.log(`Sending DELETE request to: ${url}`);
 	try {
+		loader()
+
 		await fetch(url, {
-			method : 'DELETE',
-			headers:{
-				"Content-type":"appilcation/json",
+			method: 'DELETE',
+			headers: {
+				"Content-type": "appilcation/json",
 			},
-		}).then((res)=>{
-			if(res.ok){
+		}).then((res) => {
+			removeLoader()
+			if (res.ok) {
 				console.log('http request successful');
 				toast.toastSuccess('post deleted successfully')
 				window.location.href = '/'
 			} else {
+				removeLoader()
 				toast.toastError('Post deletion Unsccessfull')
-				console.log('http request unsuccessful',res)
+				console.log('http request unsuccessful', res)
 			}
-		}).catch((err)=>{
+		}).catch((err) => {
 			toast.toastError(err.message)
 			console.log(err.message)
 		})
