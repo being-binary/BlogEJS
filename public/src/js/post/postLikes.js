c556c1ac85d48a86cb147f57e898cfeb1297b945
Abhishek Gupta <cryprincesis007@gmail.com>
2025-04-11 13:19:50 +0530

    some more changes

--- public/src/js/post/postLikes.js
+++ public/src/js/post/postLikes.js
@@ -1,6 +1,7 @@
 
   function toggleLike( event , postId) {
     event.preventDefault()
+    loader()
     // Make an AJAX request to like/unlike the post
     fetch(`https://blogejs-magw.onrender.com/post/${postId}/like`, {
         method: 'POST', // Or PUT depending on how you handle likes
@@ -10,6 +11,7 @@
     })
     .then(response => response.json())
     .then(data => {
+        removeLoader()
         if (data.success) {
             toast.toastSuccess(data.msg)
             // Reload the page or update the like count dynamically
