9e325abf0023b9d96f6eaf2b466d9cb2189f098e
Abhishek Gupta <cryprincesis007@gmail.com>
2025-04-11 12:35:31 +0530

    minor changes

--- public/src/js/loader/loader.js
+++ public/src/js/loader/loader.js
@@ -5,7 +5,7 @@ const loader = () => {
   loading.id = 'loader-screen';
   loading.style.height = '100vh';
   loading.style.width = '100%';
-  loading.style.backgroundColor = 'lightblue';
+  loading.style.backgroundColor = '#31E5F9';
   loading.style.display = 'flex';
   loading.style.justifyContent = 'center';
   loading.style.alignItems = 'center';
@@ -13,12 +13,13 @@ const loader = () => {
   loading.style.top = 0;
   loading.style.left = 0;
   loading.style.zIndex = 9999;
+  loading.style.opacity = '0.9'
 
   const text = document.createElement('p');
   text.innerHTML = 'Loading...';
-  text.style.fontSize = '24px';
+  text.style.fontSize = '36px';
   text.style.fontFamily = 'Arial, sans-serif';
-
+  text.style.color = 'white'
   loading.append(text);
   body.append(loading);
 };
